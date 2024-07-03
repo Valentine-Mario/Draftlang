99d488c79315a3370e1efb3e6f423cd4caed93d5
valentine-mario <valentine13400@gmail.com>
2024-07-03 10:05:20 +0100

    updated types

--- asdf-util/src/lib.rs
+++ asdf-util/src/lib.rs
@@ -1,5 +1,9 @@
+pub mod types;
+
+
 use draftlang_error::Error;
 use which::which;
+use types::ASDFCommand;
 
 const BINCOMMAND: &str = "asdf";
 
@@ -14,5 +18,32 @@ pub fn check_asdf() -> Result<(), Error> {
             let error_msg = format!("{:?}", e);
             return Err(Error::ASDFNotFound(string_to_static_str(error_msg)));
         }
+    }
+}
+
+
+pub fn exec_command(option: &ASDFCommand){
+    use ASDFCommand::*;
+    match option {
+        PLUGIN(_)=>todo!(),
+        CURRENT(_) => todo!(),
+        GLOBAL(_) => todo!(),
+        HELP(_) => todo!(),
+        INSTALL(_) => todo!(),
+        LATEST(_) => todo!(),
+        LIST(_) => todo!(),
+        LOCAL(_) => todo!(),
+        SHELL(_) => todo!(),
+        UNINSTALL(_) => todo!(),
+        WHERE(_) => todo!(),
+        WHICH(_) => todo!(),
+        EXEC(_) => todo!(),
+        ENV(_) => todo!(),
+        INFO => todo!(),
+        VERSION => todo!(),
+        RESHIM(_) => todo!(),
+        SHIMVERSION(_) => todo!(),
+        UPDATE => todo!(),
+        UPDATEHEAD => todo!(),
     }
 }
---
+++ asdf-util/src/types.rs
@@ -0,0 +1,22 @@
+pub enum ASDFCommand<'a> {
+    PLUGIN(&'a str),
+    CURRENT(&'a str),
+    GLOBAL(&'a str),
+    HELP(&'a str),
+    INSTALL(&'a str),
+    LATEST(&'a str),
+    LIST(&'a str),
+    LOCAL(&'a str),
+    SHELL(&'a str),
+    UNINSTALL(&'a str),
+    WHERE(&'a str),
+    WHICH(&'a str),
+    EXEC(&'a str),
+    ENV(&'a str),
+    INFO,
+    VERSION,
+    RESHIM(&'a str),
+    SHIMVERSION(&'a str),
+    UPDATE,
+    UPDATEHEAD
+}
