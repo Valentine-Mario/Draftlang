0b3e80eeed9526fc73da0cd7e0cbcc80f27dc747
valentine-mario <valentine13400@gmail.com>
2024-07-16 10:20:41 +0100

    added parsing

--- draftlang-parser/src/lib.rs
+++ draftlang-parser/src/lib.rs
@@ -1,6 +1,13 @@
+use pest::iterators::Pair;
+use pest::{error::Error, Parser};
 use pest_derive::Parser;
 
 
 #[derive(Parser)]
 #[grammar = "grammer/draftlang.pest"]
 pub struct DraftLangParser;
+
+pub fn parse_file(source: &str){
+    let pairs = DraftLangParser::parse(Rule::program, source);
+    println!("{:?}", pairs)
+}
--- draftlang-parser/src/main.rs
+++ draftlang-parser/src/main.rs
@@ -1,3 +1,6 @@
+use draftlang_parser::parse_file;
 fn main() {
-    println!("Hello, world!");
+    let unparsed_file = std::fs::read_to_string("DRAFTLANG").expect("cannot read elixir file");
+    // println!("{:?}", unparsed_file);
+    parse_file(&unparsed_file)
 }
