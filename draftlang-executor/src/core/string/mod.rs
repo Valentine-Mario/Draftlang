2c5fd2e8cecb272e0693dedf839eb7ee1f9f4caf
valentine-mario <valentine13400@gmail.com>
2024-11-11 11:58:26 +0100

    modified string func

--- draftlang-executor/src/core/string/mod.rs
+++ draftlang-executor/src/core/string/mod.rs
@@ -1,6 +1,6 @@
 use draftlang_parser::types::AstNode;
 
-mod utils;
+pub(crate) mod utils;
 
 fn check_if_string(item: &AstNode) -> bool {
     match item {
@@ -9,26 +9,10 @@ fn check_if_string(item: &AstNode) -> bool {
     }
 }
 
-fn check_if_array_of_strings(item: &AstNode) -> bool {
-    match item {
-        AstNode::Array(array_value) => array_value.iter().all(|x| match x {
-            AstNode::Str(_) => true,
-            _ => false,
-        }),
-        _ => false,
-    }
-}
 fn get_string_value(item: &AstNode) -> String {
     match item {
         AstNode::Str(value) => value.to_string(),
         _ => "".to_string(),
-    }
-}
-
-fn get_array_values(item: &AstNode) -> Vec<AstNode> {
-    match item {
-        AstNode::Array(value) => value.to_vec(),
-        _ => vec![],
     }
 }
 
@@ -37,30 +21,10 @@ fn get_array_values(item: &AstNode) -> Vec<AstNode> {
 /// # Examples
 ///
 ///
-pub fn string(ident: &str, payload: &AstNode) -> AstNode {
-    if !check_if_string(payload) {
-        panic!("unable to run operation on none string type");
-    }
-    let payload = &get_string_value(payload);
+pub fn string(ident: &str, payload: &Vec<AstNode>) -> AstNode {
     match ident {
         "str_len" => utils::str_len(payload),
-        "is_str_empty" => utils::is_str_empty(payload),
+        "str_concat" => utils::str_concat(payload),
         _ => panic!("cannot find module {} in string", ident),
-    }
-}
-
-/// The string module provides functions to manipulate array of strings
-///
-/// # Examples
-///
-///
-pub fn string_utility(ident: &str, payload: &AstNode) {
-    if !check_if_array_of_strings(payload) {
-        panic!("unable to run operation on none string array type");
-    }
-    let payload = get_array_values(&payload);
-
-    match ident {
-        _ => panic!("cannot find module {} in string utility", ident),
     }
 }
--- draftlang-executor/src/core/string/utils.rs
+++ draftlang-executor/src/core/string/utils.rs
@@ -1,9 +1,18 @@
 use draftlang_parser::types::AstNode;
 
-pub fn str_len(value: &String) -> AstNode {
+pub fn str_len(value: &Vec<AstNode>) -> AstNode {
+    if value.is_empty() {
+        panic!("Invalid parameter length. Please suply a parameter")
+    }
+    let is_str = super::check_if_string(value.first().unwrap());
+    if is_str == false {
+        panic!("Invalid parameter type. Please suply a string")
+    }
+    let value = super::get_string_value(value.first().unwrap());
     AstNode::Number(value.len() as f64)
 }
 
-pub fn is_str_empty(value: &String) -> AstNode {
-    AstNode::Boolean(value.len() < 1)
+pub fn str_concat(value: &Vec<AstNode>) -> AstNode {
+    let concat_string = value.iter().map(|x| x.to_string()).collect();
+    AstNode::Str(concat_string)
 }
