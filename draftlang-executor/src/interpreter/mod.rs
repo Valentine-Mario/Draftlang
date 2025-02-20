3e9cdd5e3fde23b29e7307cf06cb3c5ab35b7533
valentine-mario <valentine13400@gmail.com>
2025-02-20 15:55:35 +0100

    added loop execution layout

--- draftlang-executor/src/interpreter/mod.rs
+++ draftlang-executor/src/interpreter/mod.rs
@@ -44,6 +44,9 @@ impl FunctionExecxutor {
                     function_scope.insert(param.to_string(), parameters[i].clone());
                 }
             }
+            // AstNode::ForLoop { ident, range_value, expr }=>{
+
+            // }
             _ => unreachable!(),
         }
 
@@ -59,15 +62,16 @@ impl FunctionExecxutor {
             import_value: imports,
         }
     }
-   
+
     ///Execute the function body by iterating over each expression.
     ///Each expression could be either a variable assignment, an if expression,
     ///a for loop, a return statement or a function call.
     ///The function scope would be mutated accordingly.
     ///The return value would be the value of the last expression to be evaluated.
-    
+
     pub fn execute(&mut self) {
-        match &self.function_body[0] {
+        let function_body = &self.function_body.clone()[0];
+        match function_body {
             AstNode::Function {
                 name: _,
                 params: _,
@@ -79,13 +83,52 @@ impl FunctionExecxutor {
                             let variable_name = ident.to_string();
                             self.function_scope
                                 .insert(variable_name, expr.as_ref().to_owned());
-                            println!("Function Scope {:#?}", self.function_scope);
-                        },
-                        AstNode::IfExpresion(condition)=>{},
-                        AstNode::ForLoop { ident, range_value:range, expr: loop_expression }=>{
-
                         }
-                        AstNode::Return(expr)=>self.return_value=Some(*expr.clone()),
+                        AstNode::IfExpresion(condition) => {}
+                        AstNode::ForLoop {
+                            ident,
+                            range_value: range,
+                            expr: loop_expression,
+                        } => {
+                            let index = &ident.0;
+                            let value = &ident.1;
+                            let mut range_value = &AstNode::EmptyValue;
+                            let cloned_func_scope = self.function_scope.clone();
+                            let cloned_global_scope = self.global_scope.clone();
+
+                            match range.as_ref() {
+                                AstNode::Ident(ident) => {
+                                    // Attempt to retrieve the range value from the function scope
+                                    let variable_ref = cloned_func_scope.get(&ident.to_string());
+                                    match variable_ref {
+                                        // If the variable is found in the function scope, set it as the range value
+                                        Some(variable_ref) => {
+                                            range_value = variable_ref;
+                                        }
+                                        // If not found in the function scope, attempt to retrieve it from the global scope
+                                        None => {
+                                            range_value = cloned_global_scope
+                                                .get(&ident.to_string())
+                                                .unwrap();
+                                        }
+                                    }
+                                }
+                                // If the range is an array, set it as the range value
+                                AstNode::Array(_array) => {
+                                    range_value = range;
+                                }
+                                // If the range is a string, set it as the range value
+                                AstNode::Str(_str) => {
+                                    range_value = range;
+                                }
+                                //if range is neither an array or a string panic
+                                _ => {
+                                    panic!("Invalid range type");
+                                }
+                            }
+                            execute_loop(self, (index, value), loop_expression, range_value);
+                        }
+                        AstNode::Return(expr) => self.return_value = Some(*expr.clone()),
                         _ => {}
                     }
                 }
@@ -96,4 +139,16 @@ impl FunctionExecxutor {
 
     pub fn execute_function(&mut self) {}
     pub fn extract_types(&mut self) {}
+}
+
+fn execute_loop(
+    function: &mut FunctionExecxutor,
+    range_pointers: (&AstNode, &AstNode),
+    loop_body: &Vec<AstNode>,
+    range: &AstNode,
+) {
+    //iterate through the range item, and use the range pointer to update the function scope through the mut ref
+    function
+        .function_scope
+        .insert(range_pointers.0.to_string(), range.clone());
 }
--- draftlang-executor/src/main.rs
+++ draftlang-executor/src/main.rs
@@ -7,25 +7,44 @@ use color_print::cprintln;
 use draftlang_error::Error;
 
 mod util;
+/// The main entry point for the application.
+///
+/// It takes a file path as an argument and execute the draftlang
+/// file. It will parse the file, retrieve the imports and the
+/// function key from the json object, parse the function body
+/// and execute it. If the function is not found, it will
+/// panic.
+///
+///
 fn main() {
     let args = cli::Args::new();
 
     let raw_file =
         util::read_draftlang_file(&args.file_path).expect("error reading DRAFTLANG file.");
 
+    // Parse the draftlang file and execute the specified function.
     match util::parse_file(&raw_file) {
+        // If parsing is successful, proceed with execution
         Ok(ast) => {
+            // Retrieve and parse import statements from the script
             let imports = ast.script.get("import");
             let parsed_import = util::parse_imports(imports);
+
+            // Retrieve the function key from the JSON object
             let function = ast.json.data.get(&args.func);
             if function.is_none() {
                 cprintln!("<red>Function not found<red>");
                 panic!("Function not found");
             }
+
+            // Get all items in the global scope of the script
             let global_scope = ast.script.get("global").unwrap().clone();
 
+            // Iterate over each function and execute it
             function.unwrap().iter().for_each(|(name, parameters)| {
                 let function_body = ast.script.get(&name.clone().to_string()).unwrap();
+
+                // Create a new instance of FunctionExecxutor
                 let mut executor = interpreter::types::FunctionExecxutor::new(
                     ast.script.clone(),
                     parsed_import.clone(),
@@ -34,14 +53,17 @@ fn main() {
                     function_body.clone(),
                     util::parse_global_scope(global_scope.clone()),
                 );
+
+                // Execute the function
                 executor.execute();
             })
         }
+        // Handle parsing errors
         Err(err) => match err {
             Error::ParsingError(message) => {
                 cprintln!("<red>{}<red>", message)
             }
-            _ => unreachable!(),
+            _ => unreachable!(), // Other errors should not occur
         },
     }
 }
